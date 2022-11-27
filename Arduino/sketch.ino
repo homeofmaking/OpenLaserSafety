25caac54f0164e68fbe9ef7e04b252a30bd2b28c
Christoph Maser <christoph.maser+github@gmail.com>
2022-11-27 11:20:25 +0100

    actually working code ;)

--- Arduino/sketch.ino
+++ Arduino/sketch.ino
@@ -1,7 +1,7 @@
 #include <SoftwareSerial.h>
 
-#define MYPORT_TX 14
-#define MYPORT_RX 12
+#define MYPORT_TX D1
+#define MYPORT_RX D2
 
 SoftwareSerial myPort;
 
@@ -9,11 +9,13 @@ SoftwareSerial myPort;
 void setup() {
   Serial.begin(9200);
   myPort.begin(9200, SWSERIAL_8N1, MYPORT_RX, MYPORT_TX, false);
+  pinMode(D5, INPUT);
 }
 
 
 void loop() {
-  Serial.println("Loop");
-  Serial.println(myPort.readString());
-  delay(300);
+  while (myPort.available() > 0) {
+    Serial.write(myPort.read());
+    yield();
+  }
 }
